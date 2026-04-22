8f0f2a3cb2d6b330ab0580df49b2d7845660b439
Garus-Victorin <garus.victorin@gmail.com>
2026-04-22 13:19:58 +0100

    fix: add driverAdapters previewFeature to prisma generator, remove turbopack from prod config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  turbopack: {},
   serverExternalPackages: ['@prisma/client', 'bcryptjs', '@react-pdf/renderer'],
   devIndicators: {
     position: 'bottom-right',
